7847b39e5fa5d059e0bbad186c0981c9cb2bb28b
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-25 23:27:19 +0100

    added caeserCipher() functionn

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
 document.getElementById('result').innerHTML = "Hello Paragraph!";
+
+//caeserCipher()
+function caeserCipher(text, shift) {
+    let result = "";
+    for(let i = 0; i < text.length; i++) {
+        let char = text.charAt(i);
+        if(char.match(/[a-z]/i)) {
+            let isUpperCase = char === char.toUpperCase();
+            let shiftedChar = String.fromCharCode((char.charCodeAt(0) + shift - (isUpperCase ? 65: 97)) % 26 + (isUpperCase ? 65:97));
+        }
+        else {
+            result += char;
+        }
+    }
+    return result;
+}
+
+//
